f57a97e836a9fad50504367fbd973ed7623ec382
Peter Szabo <pts@fazekas.hu>
2019-01-12 19:26:43 +0100

    added compatibility with gcc-6.3, now supporting 2nd arg of operator delete

--- lib.src/operator_delete.c
+++ lib.src/operator_delete.c
@@ -10,6 +10,10 @@ static void emulate_cc_delete(void* p) {
 
 void  _ZdlPv/*operator delete  */(void* p) __attribute__((alias("emulate_cc_delete")));
 void  _ZdaPv/*operator delete[]*/(void* p) __attribute__((alias("emulate_cc_delete")));
+void  _ZdlPvm/*operator delete   amd64*/(void* p, unsigned long) __attribute__((alias("emulate_cc_delete")));
+void  _ZdaPvm/*operator delete[] amd64*/(void* p, unsigned long) __attribute__((alias("emulate_cc_delete")));
+void  _ZdlPvj/*operator delete   i386*/(void* p, unsigned int) __attribute__((alias("emulate_cc_delete")));
+void  _ZdaPvj/*operator delete[] i386*/(void* p, unsigned int) __attribute__((alias("emulate_cc_delete")));
 
 #ifdef __cplusplus
 }  /* extern "C" */
